c65c61d6928e67f472d7461b40be2c332ed29e16
Dragos Tanta <dragos.tanta@gmail.com>
2024-03-08 16:54:52 +0200

    curs3 intermediar

---
+++ src/curs3/TestOddOrEven.java
@@ -0,0 +1,15 @@
+package curs3;
+
+public class TestOddOrEven {
+
+	public static void main(String[] args) {
+
+		
+		CheckOddOrEven obj = new CheckOddOrEven();
+		//obj.askTheUserForANumber();
+		obj.checkNumberIsOddOrEven();
+		
+		
+	}
+
+}
