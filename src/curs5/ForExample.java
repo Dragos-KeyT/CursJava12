f18046d6c9aed256e3c9f6520956dae97b9f4fa6
Dragos Tanta <dragos.tanta@gmail.com>
2024-03-15 16:38:57 +0200

    curs5 intermediar

--- src/curs5/ForExample.java
+++ src/curs5/ForExample.java
@@ -4,6 +4,19 @@ public class ForExample {
 
 	public static void main(String[] args) {
 
+		
+		for(int i =0; i <10; i++) {
+		
+			if(i == 5) {
+				//break;
+				continue;
+			}
+			
+			System.out.println("Valoarea lui i este " + i);
+			
+		}
+		
+		
 	}
 
 }
